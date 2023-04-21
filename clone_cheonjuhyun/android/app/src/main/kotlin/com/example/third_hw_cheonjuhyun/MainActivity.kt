499cf351ac15a1cf10530a49bd856a2cecab6849
millejuice <potgusdavy@gmail.com>
2023-04-21 19:18:01 +0900

    init git

---
+++ clone_cheonjuhyun/android/app/src/main/kotlin/com/example/third_hw_cheonjuhyun/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.third_hw_cheonjuhyun
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- second_homework_chjh/lib/main.dart
+++ second_homework_chjh/lib/main.dart
@@ -1,46 +1,67 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/textstyle.dart';
-
-import 'secondpage.dart';
+import 'data.dart'; // data.dart의 정보를 import 한다.
 
 void main() {
-  runApp(
-    MaterialApp(
-      title: 'Named Routes Demo',
-// Start the app with the "/" named route. In this case, the app starts
-// on the FirstScreen widget.
-      initialRoute: '/',
-      routes: {
-// When navigating to the "/" route, build the FirstScreen widget.
-        '/': (context) => const FirstScreen(),
-// When navigating to the "/second" route, build the SecondScreen widget.
-        '/second': (context) => const SecondScreen(),
-      },
-    ),
-  );
+  runApp(const MyApp());
 }
 
-class FirstScreen extends StatelessWidget {
-  const FirstScreen({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  Future<List<String>> _getData() async {
+    // 1. Future를 통해 data.dart의 리스트 정보를 가져올 때 async, await와 Future.delayed를 활용하여 10초의 정지 시간을 갖도록 한다.
+    // 2. 10초가 지난 후 리스트 정보를 가져왔다면 then()을 활용하여 console창에 ‘출력 완료’ 라는 text가 출력하도록 만들어준다.
+    return await Future.delayed(const Duration(seconds: 10), () {
+      return myData;
+    }).then((val) {
+      print('출력완료!');
+      return val;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('First Screen'),
+        title: Text('My Page'),
       ),
-      body: Center(
-        child: ElevatedButton(
-// Within the `FirstScreen` widget
-          onPressed: () {
-// Navigate to the second screen using a named route.
-            Navigator.pushNamed(context, '/second');
-          },
-          child: Text(
-            'Launch screen',
-            style: title1Style(color: Colors.red),
-          ),
-        ),
+      body: FutureBuilder<List<String>>(
+        future: _getData(),
+        builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data?.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index]),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text('Loading........'),
+            );
+          }
+        },
+        // 3. FutureBuilder<List<String>>와 Listview.Builder를 활용하여 아래 ListTile 코드를 통해 결과 화면과 같은 화면을 출력한다.
       ),
     );
   }
